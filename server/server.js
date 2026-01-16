f7488976c439572f43badb9714281fed466a66a8
Chinonso Chikelue <163886675+chinonsochikelue@users.noreply.github.com>
2026-01-16 21:26:09 +0100

    fix: update CORS origin to use the correct production URL for the client application

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5175", "https://trendy-jet.vercel.app"],
+    origin: ["http://localhost:5173", "http://localhost:5175", "https://bespoke-tailor.vercel.app"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
